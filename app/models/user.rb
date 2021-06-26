7e608ebc4494a573c0aa8177f61aaafbcadc9d9f
Marcelo <marcelocdias93@gmail.com>
2021-06-26 14:20:54 -0300

    Downcase user email before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   validates :username, presence: true,
@@ -11,4 +13,10 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX }
 
   has_many :articles
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
