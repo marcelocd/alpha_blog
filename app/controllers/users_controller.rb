0ee6b9f1edef93d9b05f415f3cd12bc0b6d236e8
Marcelo <marcelocdias93@gmail.com>
2021-06-26 23:42:28 -0300

    Add account editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(valid_params)
-    if @user.save
+    if @user.save!
 			flash[:notice] =
         "Welcome to the Alpha Blog, #{@user.username}! You have successfully signed up."
 			redirect_to articles_path
@@ -14,7 +16,22 @@ class UsersController < ApplicationController
 		end
   end
 
+  def edit ; end
+
+  def update
+    if @user.update!(valid_params)
+			flash[:notice] = "Your account information was successfully updated!"
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+  end
+
   private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 
   def valid_params
     params.require(:user)
