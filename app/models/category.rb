bbe97815a0da80ad995faddc79895b9846a1a17e
Marcelo <marcelocdias93@gmail.com>
2021-06-30 10:57:44 -0300

    Validations using unit tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  validates :name, presence: true,
+                   uniqueness: true,
+                   length: { minimum: 3,
+                             maximum: 25 }
 end
